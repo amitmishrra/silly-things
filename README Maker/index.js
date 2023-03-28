ac933ed8d76555b85721f9bf2b86dff4a8977a94
mishra4amit4@gmail.com <mishra4amit4@gmail.com>
2023-03-29 00:48:40 +0530

    sdfg

--- README Maker/index.js
+++ README Maker/index.js
@@ -1,52 +1,51 @@
-const fs = require('fs'); // Require the File System module
-const json = require("./a.json"); // The path to the JSON file
-const data = 'Hello, World!'; // The text you want to write in the file
-
+const fs = require('fs'); 
+const json = require("./a.json"); 
+const data = 'Hello, World!'; 
 
 for (let i = 0; i < json.length; i++) {
-    fs.appendFileSync('example.txt', `\n # ${i + 1} ${json[i].name}`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n # ${i + 1} ${json[i].name}`, (err) => {  
         if (err) throw err;
     });
 
-    fs.appendFileSync('example.txt', `\n ${json[i].description}`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n ${json[i].description}`, (err) => {  
         if (err) throw err;
     });
 
-    fs.appendFileSync('example.txt', `\n ## ⚙️ Tech Stack`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n ## ⚙️ Tech Stack`, (err) => {  
         if (err) throw err;
     });
 
     for (let j = 0; j < json[i].tech.length; j++) {
-        fs.appendFileSync('example.txt', `\n* ${json[i].tech[j]}`, (err) => { // Write the file
+        fs.appendFileSync('example.txt', `\n* ${json[i].tech[j]}`, (err) => {  
             if (err) throw err;
         });
     }
 
-    fs.appendFileSync('example.txt', `\n ## 🔗 Links`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n ## 🔗 Links`, (err) => {  
         if (err) throw err;
     });
 
-  fs.appendFileSync('example.txt', `\n * ${json[i].repo}`, (err) => { // Write the file
+  fs.appendFileSync('example.txt', `\n * ${json[i].repo}`, (err) => {  
         if (err) throw err;
     });
 
     if (json[i].hasOwnProperty("liveLink")) {
-        fs.appendFileSync('example.txt', `\n * ${json[i].liveLink}`, (err) => { // Write the file
+        fs.appendFileSync('example.txt', `\n * ${json[i].liveLink}`, (err) => {  
             if (err) throw err;
         });
     }
 
-    fs.appendFileSync('example.txt', `\n`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n`, (err) => {  
         if (err) throw err;
     });
 
-    fs.appendFileSync('example.txt', `\n`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n`, (err) => {  
         if (err) throw err;
     });
-    fs.appendFileSync('example.txt', `\n`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n`, (err) => {  
         if (err) throw err;
     });
-    fs.appendFileSync('example.txt', `\n`, (err) => { // Write the file
+    fs.appendFileSync('example.txt', `\n`, (err) => {  
         if (err) throw err;
     });
 
